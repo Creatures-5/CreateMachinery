1dbd5e2d042d9b0df9ca3f4320a0e582225910e5
Creatures-5 <43184048+Creatures-5@users.noreply.github.com>
2026-04-08 11:43:16 -0700

    Replaced diamon & netherite drills with brass & sturdy drills, textures updated accordingly

--- common/src/main/java/create_machinery/Items.java
+++ common/src/main/java/create_machinery/Items.java
@@ -32,8 +32,8 @@ public interface Items {
 
     Supplier<Item> REDSTONE_MECHANISM = register("redstone_mechanism", () -> new Item(new Item.Properties().stacksTo(64)));
     Supplier<Item> IRON_DRILL = register("iron_drill", () -> new Item(new Item.Properties().stacksTo(8)));
-    Supplier<Item> DIAMOND_DRILL = register("diamond_drill", () -> new Item(new Item.Properties().stacksTo(8)));
-    Supplier<Item> NETHERITE_DRILL = register("netherite_drill", () -> new Item(new Item.Properties().stacksTo(8)));
+    Supplier<Item> BRASS_DRILL = register("brass_drill", () -> new Item(new Item.Properties().stacksTo(8)));
+    Supplier<Item> STURDY_DRILL = register("sturdy_drill", () -> new Item(new Item.Properties().stacksTo(8)));
 
     static void bootstrap() {
     }
